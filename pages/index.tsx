3160b6c5038c15512778bf28425ec4c1c31a5898
SamyakJain2020 <samyakjainking@gmail.com>
2022-04-27 23:09:41 +0530

    initial commit -2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
-import Navbar from '../components/Navbar'
+
 import HomeSection from '../components/Home'
 const Home: NextPage = () => {
   return (
-    <div >
-      <Navbar></Navbar>
+    <div>
       <HomeSection/>
     </div>
   )
